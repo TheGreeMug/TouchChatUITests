fcc010d8cc8d42ed94671e25e9fdbc7ab6587d31
Marius N <n.marius@hotmail.com>
2024-06-06 13:23:09 +0300

    commit for test with changes

--- 3_backup.sh
+++ 3_backup.sh
@@ -3,12 +3,25 @@
 # Step 1: Go up one folder
 cd ..
 
-# Step 2: Revert the GIT changes to TouchChat.xcodeproj/project.pbxproj
-git checkout -- TouchChat.xcodeproj/project.pbxproj
+# Define paths and timestamp
+SOURCE_PATH="TouchChat.xcodeproj/project.pbxproj"
+BACKUP_DIR="TouchChatUITests/backups"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+# Step 2: Create a copy of the file in TouchChatUITests/backups and overwrite existing files if any
+cp "$SOURCE_PATH" "$BACKUP_DIR/project.pbxproj"
+echo "Current project.pbxproj copied to $BACKUP_DIR/project.pbxproj"
+
+# Step 3: Create a backup of the file with YES changes in TouchChatUITests/backups directory
+BACKUP_YES_PATH="$BACKUP_DIR/project_backupYESChanges_$TIMESTAMP.pbxproj"
+cp "$SOURCE_PATH" "$BACKUP_YES_PATH"
+echo "Backup with YES changes created at $BACKUP_YES_PATH"
+
+# Step 4: Revert the GIT changes to TouchChat.xcodeproj/project.pbxproj
+git checkout -- "$SOURCE_PATH"
 echo "Reverted Git changes to TouchChat.xcodeproj/project.pbxproj"
 
-# Step 3: Create a backup of the file in TouchChatUITests directory
-SOURCE_PATH="TouchChat.xcodeproj/project.pbxproj"
-BACKUP_PATH="TouchChatUITests/project_backup.pbxproj"
-cp "$SOURCE_PATH" "$BACKUP_PATH"
-echo "Backup created at $BACKUP_PATH"
+# Step 5: Create a backup of the file with NO changes in TouchChatUITests/backups directory
+BACKUP_NO_PATH="$BACKUP_DIR/project_backupNOChanges_$TIMESTAMP.pbxproj"
+cp "$SOURCE_PATH" "$BACKUP_NO_PATH"
+echo "Backup with NO changes created at $BACKUP_NO_PATH"
